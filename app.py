1ca9a5b080f546d0061bda18e5806e6f808a4609
Ubuntu <ubuntu@ip-172-31-34-30.ec2.internal>
2020-03-13 15:46:08 +0000

    added base.html receipt.html and addreceipt

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'receipt_book'
+app.config["MONGO_URI"] = 'mongodb+srv://mongouser:<bbmongo20>@myfirstcluster-km3xj.mongodb.net/receipt_book?retryWrites=true&w=majority'
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_receipts')
+def get_receipts():
+    return render_template("receipts.html", receipts=mongo.db.receipts.find())
 
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find(),
+                           allergens=mongo.db.allergens.find())
+    
+@app.route('/insert_receipt', methods=['POST'])
+def insert_receipt():
+    receipts = mongo.db.receipts
+    receipts.insert_one(request.form.to_dict())
+    return redirect(url_for('get_receipts'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
